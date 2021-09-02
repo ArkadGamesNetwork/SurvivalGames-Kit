eeff9c8295cd1def17bde017f446d9634771dda3
MrCubee <contact@mrcubee.fr>
2021-09-02 18:05:29 +0200

    [add] auto furnace skill in Miner kit.

--- src/fr/mrcubee/survivalgames/kit/list/MinerKit.java
+++ src/fr/mrcubee/survivalgames/kit/list/MinerKit.java
@@ -1,15 +1,25 @@
 package fr.mrcubee.survivalgames.kit.list;
 
 import fr.mrcubee.langlib.Lang;
+import fr.mrcubee.survivalgames.GameStats;
 import fr.mrcubee.survivalgames.SurvivalGamesAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import fr.mrcubee.survivalgames.kit.Kit;
+
+import java.util.Iterator;
 
 public class MinerKit extends Kit {
 
@@ -22,11 +32,10 @@ public class MinerKit extends Kit {
 		this.items = new ItemStack[] {
 				new ItemStack(Material.STONE_PICKAXE)
 		};
-		this.items[0].addEnchantment(Enchantment.DURABILITY, 2);
 		this.items[0].addEnchantment(Enchantment.DIG_SPEED, 5);
-		this.items[0].addEnchantment(Enchantment.DURABILITY, 3);
 		itemMeta = this.items[0].getItemMeta();
 		itemMeta.setDisplayName(ChatColor.RED + "Miner");
+		itemMeta.spigot().setUnbreakable(true);
 		this.items[0].setItemMeta(itemMeta);
 	}
 
@@ -76,5 +85,37 @@ public class MinerKit extends Kit {
 	@Override
 	public void update() {
 
+	}
+
+	private boolean blockBreakRecipe(BlockBreakEvent event, Recipe recipe) {
+		FurnaceRecipe furnaceRecipe;
+		Block block;
+		World world;
+
+		if (event == null || !(recipe instanceof FurnaceRecipe))
+			return false;
+		furnaceRecipe = (FurnaceRecipe) recipe;
+		if (!event.getBlock().getType().equals(furnaceRecipe.getInput().getType()))
+			return false;
+		event.setCancelled(true);
+		block = event.getBlock();
+		block.setType(Material.AIR);
+		world = block.getWorld();
+		world.dropItemNaturally(event.getBlock().getLocation().add(0.5, 0.5, 0.5), furnaceRecipe.getResult());
+		return true;
+	}
+
+	@EventHandler
+	public void blockBreakEvent(BlockBreakEvent event) {
+		Iterator<Recipe> recipeIterator;
+		Recipe recipe;
+
+		if (SurvivalGamesAPI.getGame().getGameStats() != GameStats.DURING
+		|| !getPlayers().contains(event.getPlayer()))
+			return;
+		recipeIterator = Bukkit.getServer().recipeIterator();
+		if (recipeIterator == null)
+			return;
+		while (recipeIterator.hasNext() && !blockBreakRecipe(event, recipeIterator.next()));
 	}
 }
